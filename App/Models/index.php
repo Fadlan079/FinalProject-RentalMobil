4512b80f019c1243aad6fa24f57e0aff73023230
nathan andik <24_nathandika@student.smkti.net>
2025-10-09 12:07:24 +0800

    CRUD mobil

--- App/Models/index.php
+++ App/Models/index.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once "user.php";
+require_once "mobil.php";
 
-$user1 = new User();
-$user1->Insertuser("Fadlan","SIM A", "fadlanawas@gmail.com", "Admin123");
+// $user1 = new User();
+// $user1->Insertuser("Fadlan","SIM A", "fadlanawas@gmail.com", "Admin123");
+
+// $mobil = new Mobil();
+// $mobil->InsertMobil("2", "warna hitam", "Koenigsegg", "7000000", "Manual","Bensin");
 ?>
