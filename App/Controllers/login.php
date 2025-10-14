e56a8e86c145cffc9879acda203bffdc352ca568
Fattah <glipxse@gmail.com>
2025-10-14 15:21:53 +0800

    LAST VALIDASI LOGIN NAMA/EMAIL

--- App/Controllers/login.php
+++ App/Controllers/login.php
@@ -67,15 +67,11 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
     <h2 class="text-2xl font-bold mb-6 text-center text-gray-800">Login</h2>
     
     <div class="mb-4">
-      <input type="text" name="nama" placeholder="Username..." class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500" />
-    </div>
-
-    <div class="mb-4">
-      <input type="email" name="email" placeholder="Email..." class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500" />
+      <input type="text" name="identifier" placeholder="Nama / Email" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500" />
     </div>
 
     <div class="mb-6">
-      <input type="password" name="pass" placeholder="Password..." class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500" />
+      <input type="password" name="password" placeholder="Password..." class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500" />
     </div>
 
     <input type="submit" value="Send"
