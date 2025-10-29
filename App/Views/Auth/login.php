52a415fc9c057bed46b51251997008e25a2e6a34
Fadlan079 <fadlanfirdaus220@gmail.com>
2025-10-29 22:18:18 +0800

    Styling Login,Signup

--- App/Views/Auth/login.php
+++ App/Views/Auth/login.php
@@ -4,22 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Rent Car Cylc</title>
-    <link rel="stylesheet" href="../src/output.css">
+    <link rel="stylesheet" href="output.css">
 </head>
-<body class="bg-[url('../src/assets/Jesko%20absolut.jpeg')] bg-cover bg-center bg-no-repeat h-screen flex justify-center items-center">
-    <form action="login.php" method="POST"class="bg-indigo-50 p-8 rounded-lg shadow-lg w-full max-w-sm">
-    
-    <h2 class="text-2xl font-bold mb-6 text-center text-gray-800">Login</h2>
-    
-    <div class="mb-4">
-      <input type="text" name="identifier" placeholder="Nama / Email / Telepon" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500" />
-    </div>
-
-    <div class="mb-6">
-      <input type="password" name="password" placeholder="Password" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-slate-500" />
-    </div>
-
-    <input type="submit" value="Send" class="w-full bg-orange-500 text-white py-2 rounded-lg hover:bg-orange-400 transition duration-200 focus:outline-none focus:ring-2 focus:ring-orange-600 cursor-pointer transition-allduration-3" />
+<body>
+  <form action="index.php?action=storelogin" method="post">
+      <label>Email</label>
+      <input name="email" type="text" placeholder="Example@gmail.com" required>
+      <label>Password</label>
+      <input name="password" type="password" placeholder="********" required>
+      <input type="submit" value="Kirim">
+      <a href="index.php?action=signup">Belum Punya Akun? SignUp</a>
   </form>
 </body>
 </html>
--- App/Views/Auth/signup.php
+++ App/Views/Auth/signup.php
@@ -3,9 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>SignUp - Cylc Rent Car</title>
+    <link rel="stylesheet" href="output.css">
 </head>
 <body>
-    
+    <form action="index.php?action=storesignup" method="post">
+        <label>Email</label>
+        <input name="email" type="email" placeholder="Example@gmail.com" required>
+        <label>Password</label>
+        <input name="password" type="password" placeholder="********" required>
+        <br>
+        <label>Jenis kelamin</label>
+        <br>
+        <select name="jk">
+            <option value="L">Laki-laki</option>
+            <option value="P">Perempuan</option>
+        </select>
+        <input type="submit" value="Kirim">
+        <a href="index.php?action=login">Sudah Punya Akun? Login</a>
+    </form>
 </body>
 </html>
