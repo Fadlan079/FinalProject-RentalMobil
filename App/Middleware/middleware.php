ba815adcf7937f7342e3786f891662967d9beec6
Fadlan079 <fadlanfirdaus220@gmail.com>
2025-11-02 22:13:12 +0800

    Feature/middleware(require login,require role)

---
+++ App/Middleware/middleware.php
@@ -0,0 +1,22 @@
+<?php
+class Middleware{
+    public static function requirelogin(){
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (!isset($_SESSION['user'])) {
+            header("Location: ../Public/?action=index");
+            exit;
+        }
+    }
+
+    public static function requirerole($role){
+        self::requirelogin();
+        if ($_SESSION['user']['role'] !== $role) {
+            header("Location: ../Public/?action=index");
+            exit;
+        }
+    }
+}
+?>
